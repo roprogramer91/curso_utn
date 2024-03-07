5dfca3a4bfab76d9c43dd097315508b0995dea4a
roprogramer91 <roprogramer91@gmail.com>
2024-03-06 22:18:44 -0300

    Se agrega libreria javascript Lightbox para visualizacion de las imagenes en carrusel.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actividad integradora</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/lightbox.min.css">
 </head>
 <body>
     
@@ -23,7 +24,7 @@
     <div class="secciones">
         <section id="homero" class="Seccion">
             <h2>Homero Jay Simpson</h2>
-            <img src="images/Homer_Simpson.webp" alt="Homero Simpson" srcset="">
+            <a href="images/Homer_Simpson.webp" data-lightbox="roadtrip" data-title="Homero Simpson"><img src="images/Homer_Simpson.webp" alt="Homero Simpson"></a>
             <p>También es llamado Max Power en el episodio Homer to the Max.</p>
             <p>Básicamente, Homer es una sátira de los norteamericanos promedio, en palabras de su mismo creador Matt Groening. Las bromas indirectas se pueden encontrar en su personalidad, su actitud, su filosofía, su idiosincrasia, etc. 
             <p>Los rasgos esenciales de la personalidad de Homer se presentaron desde sus inicios. No se sabe bien donde nació, pero pudo haber sido en Springfield. Después de los 11 años fue criado únicamente por su padre Abraham Simpson, ya que su madre tuvo que huir de la justicia por paralizar los negocios del Sr. Burns.</p> 
@@ -36,7 +37,7 @@
 
         <section id="marge" class="Seccion">
             <h2>Marjorie "Marge" Jaqueline Bouvier Simpson</h2>
-            <img src="images/Marge_Simpson.webp" alt="" srcset="">
+            <a href="images/Marge_Simpson.webp" data-lightbox="roadtrip" data-title="Marge Simpson"><img src="images/Marge_Simpson.webp" alt="MArge Simpson"></a>
             <p>Marge se crio en una zona rural de la Norteamérica sureña, en un ambiente muy familiar. Se crio en una granja e iba de vacaciones con toda la familia a Barnacle Bay, una isla en la costa de Springfield. Su padre era asistente de vuelo (Azafato), cosa que traumatizó a la joven Marge, desarrollando miedo a los aviones y a volar.  </p>
             <p>Pronto Marge se fue a vivir a la ciudad de Springfield y se graduó en el instituto. Al finalizar sus estudios obligatorios, Marge había tenido varios amores, pero Homer, que ya lo conocía de un campamento en su niñez (aunque no sabía que fuera él), sería con el que tendría una relación más larga y acabaría casándose. Marge trabajó de camarera en un autoservicio mientras era soltera y se graduó en la universidad, donde peligró su relación con su novio Homer cuando se enamoró de uno de los profesores.</p>
             <p>El personaje de Marge Simpson ha ahondado en los roles arquetípicos que suelen desempeñar las esposas amas de casa en las series de animación, en la misma línea promovida por Hanna Barbera con Vilma Picapiedra o Jane Jetson. Su apasionada y ciega dedicación a su desagradecida familia ha servido de modelo moderno para otros personajes femeninos en series de animación posteriores. Por ello, ha sido considerada como la mejor madre tanto ficticia como real por el público británico.</p>
@@ -44,7 +45,7 @@
 
         <section id="bart" class="Seccion">
             <h2>Bartolomeo Jay Simpson Bouvier</h2>
-            <img src="images/Bart_Simpson.webp" alt="">
+            <a href="images/Bart_Simpson.webp" data-lightbox="roadtrip" data-title="Bart Simpson"><img src="images/Bart_Simpson.webp" alt="Bart Simpson"></a>
             <p>Bart es decididamente el más rebelde y travieso de la familia. Es un muchacho simpático y también muy travieso, que hace muchas bromas con su mejor amigo Milhouse. Sigue los programas de su ídolo Krusty el Payaso. Es desobediente y hace todo lo que le pasa por la cabeza. Su nombre es un anagrama de "brat", que en inglés significa "travieso". Su ideología es "yo no fui, nadie me vio, no pueden demostrarlo". También en un capítulo Seymour Skinner descubre que es alérgico al camarón.</p>
             <p>La primera palabra de Bart fue "¡Ay Caramba!", así se ve en "La Primera palabra de Maggie" pues vio a sus padres teniendo sexo, y de la impresión gritó "¡¡Ay caramba!!" y tiró el biberón.</p>
             <p>En el transcurso de la serie se puede ver que en las primeras temporadas era indiscutiblemente el personaje principal, pero pasando las temporadas, queda relegado a un papel secundario, dado que su padre Homer asume el rol de personaje principal. Esto ocurre porque Homer hace más fácil la auto identificación con el telespectador promedio de los Simpson, además de que un guión ceñido al personaje adulto hace más accesible la crítica social que tiene que la serie realice. Bart una vez fue enviado a una Escuela Militarizada donde demostró increíble manejo con las armas, prueba de ello es que logró disparar un cohete contra el estacionamiento del director Skinner estando a una distancia considerable.</p>
@@ -52,7 +53,7 @@
 
         <section id="lisa" class="Seccion">
             <h2>Lisa Marie Simpson</h2>
-            <img src="images/Lisa_Simpson.webp" alt="Lisa Simpson" srcset="">
+            <a href="images/Lisa_Simpson.webp" data-lightbox="roadtrip" data-title="Lisa Simpson"><img src="images/Lisa_Simpson.webp" alt="Lisa Simpson"></a>
             <p>(A veces llamada Liz por su hermano Bart), es un personaje principal de la serie de televisión animada Los Simpson. Es la hija mediana de Homer y Marge Simpson, y la hermana de Bart y Maggie. Goza de notable protagonismo y complejidad en la serie aunque es uno de los personajes principales menos popular.
            </p>
            <p>Lisa Simpson representa la incansable activista frustrada, ha servido como estandarte para causas ecológicas, y tambien una defensora de causas feministas es el vehículo para proyectar la serie hacia nuevas ideologías y religiones orientales; por lo que es considerada como el "centro ético y moral de la Familia Simpson"</p>
@@ -60,7 +61,7 @@
 
         <section id="maggie" class="Seccion">
             <h2>Margaret "Maggie" Abigail Simpson Bouvier</h2>
-            <img src="images/Maggie_Simpson.webp" alt="">
+            <a href="images/Maggie_Simpson.webp" data-lightbox="roadtrip" data-title="Maggie Simpson"><img src="images/Maggie_Simpson.webp" alt="Maggie Simpson"></a>
             <p>Maggie es la tercera y la hija más joven del matrimonio protagonista, Homer Simpson y Marge Simpson, con apenas dos años de vida. Sus hermanos mayores son Bart Simpson y Lisa Simpson. Debido a que aún no puede hablar, Maggie es la que menos protagonismo e importancia tiene en la familia Simpson y en la serie. Siempre se la ve succionando su chupete y cuando camina suele tropezar y cae de rodillas. Maggie fue creada por el dibujante Matt Groening en el vestíbulo de la oficina de James L. Brooks, para debutar en la televisión el 19 de abril de 1987, en un corto para el programa El show de Tracey Ullman "Good Night".</p>
             <p>Maggie tiene una gran capacidad de comunicación y a pesar de ser un bebe, ha demostrado ser muy inteligente. Ella no tiene aprecio por casi ningún otro niño, en especial por Gerald, el bebé de una sola ceja, por lo tanto no tiene amigos pero se le ve ayudando en la guardería en el episodio "Un tranvía llamado Marge". Su primera palabra fue "papi", la que dijo en el episodio "La primera palabra de Maggie".</p>
             <p>Maggie es la favorita de Homer, el cual la ignora muchas veces o no sabe que existe en algunos capítulos. Pero se descubre el amor de Homer hacia ella en el capitulo en el que relata que no hay fotos de Maggie en la casa ya que esta en su sección de la planta nuclear reemplazando el letrero que en inglés dice "no lo olvide, está aquí para siempre" por "hazlo por ella" (un juego de palabras intraducible del inglés, que del "don´t forget: you are here for ever", pasa a "do it for her" borrando el resto de las letras). </p>
@@ -84,5 +85,7 @@
     <footer>
         <p>&copy; 2024 Roger Ramirez</p>
     </footer>
+
+    <script src="js/lightbox-plus-jquery.min.js"></script>
 </body>
 </html>
